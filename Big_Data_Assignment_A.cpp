3e1d6d66466eeaac78280265839b79c26b224a88
Haotian Wu <lswht17@gmail.com>
2015-02-09 23:16:11 -0500

    Initial version of the scrub program.

---
+++ Big_Data_Assignment_A.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Big_Data_Assignment_A.cpp
+// Author      : Haotian Wu
+// Version     :
+// Copyright   : Copy Riget Reserved by Haotian Wu
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
